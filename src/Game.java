885efebc51aa1814e09f028f27146379a1c68d15
Matthew Williams (maw101) <maw101@aber.ac.uk>
2019-07-15 16:49:31 +0100

    Implemented valid move checks

--- src/Game.java
+++ src/Game.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class Game {
 
     private char[][] grid;
+    private int boardSize;
 
     public Game() {
-        grid = new char[3][3];
+        boardSize = 3;
+        grid = new char[boardSize][boardSize];
     }
 
     public char[][] getNewGrid() {
-        return new char[3][3];
+        return new char[boardSize][boardSize];
     }
 
     public void renderGrid() {
         System.out.println("  0 1 2");
         System.out.println("  -----");
-        for (int row = 0; row < 3; row++) {
+        for (int row = 0; row < boardSize; row++) {
             System.out.print(row + "|");
-            for (int col = 0; col < 3; col++) {
+            for (int col = 0; col < boardSize; col++) {
                 if (grid[col][row] == 0)
                     System.out.print("  ");
                 else
@@ -28,7 +30,7 @@ public class Game {
     }
 
     // todo: better input method to prevent exceptions etc
-    public Coordinate getPlayerMove() {
+    private Coordinate getCoordinateInput() {
         Scanner in = new Scanner(System.in);
         Coordinate position = new Coordinate();
 
@@ -43,8 +45,26 @@ public class Game {
         return position;
     }
 
-    public void makeMove() {
+    private boolean isValidMove(Coordinate move) {
+        if ((move.getX() < 0 || move.getX() >= boardSize) ||
+                (move.getY() < 0 || move.getY() >= boardSize)) {
+            System.err.println("Invalid Coordinate - " + move + " - outside the grid");
+            return false;
+        } else if ((grid[move.getY()][move.getX()] == 0)) { // if == 0 means empty square
+            System.err.println("Square " + move + " is already occupied ");
+            return false;
+        }
+        return true;
 
+    }
+
+    public void makeMove(char player) {
+        Coordinate move;
+        do {
+            move = getCoordinateInput();
+        } while (!isValidMove(move));
+
+        grid[move.getY()][move.getX()] = player;
     }
 
 }
