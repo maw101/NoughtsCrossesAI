4fed1ec6edc1579374cadd66da024b796bc1686e
Matthew Williams (maw101) <maw101@aber.ac.uk>
2019-07-15 20:17:46 +0100

    split down into new methods

--- src/WinningMoveAI.java
+++ src/WinningMoveAI.java
@@ -8,18 +8,29 @@ public class WinningMoveAI implements AI {
 
     @Override
     public Coordinate makeMove(char[][] grid, int[] moveSums, char player) {
+        Coordinate position = null;
+
+        position = getWinningMoveForGivenPlayer(grid, moveSums, player);
+
+        if (position == null) // unable to find a winning move so choose a position randomly
+            position = getRandomMove(grid);
+
+        return position;
+    }
+
+    private Coordinate getWinningMoveForGivenPlayer(char[][] grid, int[] moveSums, char player) {
+        Coordinate position = null;
         int boardSize = grid.length;
         int countLookingFor = grid.length - 1; // looking for a count where we are 1 off n in a row
-        Coordinate position = null;
 
         if (player == 'O')
             countLookingFor *= -1;
 
         // try to find a line where we are 1 off n in a row
-            // we utilise moveSums which is used to store the count for the player occupying
-            //  the most positions on the current winning line. rows are followed by columns
-            //  which are then followed by the diagonals - this is detailed in Game.java.
-            //  'X' gives a positive value in moveSums and 'O' gives a negative value in it.
+        // we utilise moveSums which is used to store the count for the player occupying
+        //  the most positions on the current winning line. rows are followed by columns
+        //  which are then followed by the diagonals - this is detailed in Game.java.
+        //  'X' gives a positive value in moveSums and 'O' gives a negative value in it.
         for (int moveNum = 0; moveNum < moveSums.length; moveNum++)
             if (moveSums[moveNum] == countLookingFor) { // found a line where we are one off winning
                 if (moveNum < boardSize) { // is row
@@ -41,14 +52,15 @@ public class WinningMoveAI implements AI {
                 }
             }
 
-        if (position == null) { // unable to find a winning move so choose a position randomly
-            position = new Coordinate();
-            do {
-                position.setX(rand.nextInt(boardSize));
-                position.setY(rand.nextInt(boardSize));
-            } while (grid[position.getX()][position.getY()] != 0); // if != 0 means occupied square
-        }
+        return position;
+    }
 
+    private Coordinate getRandomMove(char[][] grid) {
+        Coordinate position = new Coordinate();
+        do {
+            position.setX(rand.nextInt(grid.length));
+            position.setY(rand.nextInt(grid.length));
+        } while (grid[position.getX()][position.getY()] != 0); // if != 0 means occupied square
         return position;
     }
 
