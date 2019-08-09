e6ccb1a2664e01d5b3e25662de24b42806902d02
maw101 <maw101@aber.ac.uk>
2019-08-09 10:50:38 +0100

    Implemented Player class to hold players symbol and the name of the algorithm that player is using (so human or one of the AIs)

---
+++ src/Player.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Player {
+
+    private char symbol;
+    private String algorithmName;
+
+    public Player(char symbol, String algorithmName) {
+        this.symbol = symbol;
+        this.algorithmName = algorithmName;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getAlgorithmName() {
+        return algorithmName;
+    }
+
+    public void setAlgorithmName(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
+
+    @Override
+    public String toString() {
+        return "Player with symbol '" + symbol +
+                "' using algorithm '" + algorithmName + "'.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return symbol == player.symbol &&
+                Objects.equals(algorithmName, player.algorithmName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(symbol, algorithmName);
+    }
+
+}
