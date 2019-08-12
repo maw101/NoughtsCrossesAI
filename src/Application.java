75bdb86a83d6616cc59ded3f2565d36d8bc6bf9e
maw101 <maw101@aber.ac.uk>
2019-08-12 12:30:51 +0100

    Changed to test OptimisedAI class

--- src/Application.java
+++ src/Application.java
@@ -2,8 +2,8 @@ class Application {
 
     public static void main(String[] args) throws Exception {
         Game g = new Game();
-        g.runBattles(500000, "RandomAI", "FindWinningBlockLosingAI");
-        //g.play(args[0], args[1]);
+        //g.runBattles(1000, "FindWinningBlockLosingAI", "OptimisedAI");
+        g.play(args[0], args[1]);
     }
 
 }
